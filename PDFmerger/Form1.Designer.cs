305f33e098341862857cba845888aa35cbc8264d
hagiAyato <h8a9g11i.@gmail.com>
2020-10-18 23:55:52 +0900

    UI機能追加(一番上に移動、一番下に移動) ※一番下に移動は移動先の順番整列がうまくいかない…?

--- PDFmerger/Form1.Designer.cs
+++ PDFmerger/Form1.Designer.cs
@@ -35,24 +35,34 @@
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
+            this.FileName = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.FilePath = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
+            this.dataGridView1.AllowDrop = true;
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AllowUserToOrderColumns = true;
             this.dataGridView1.AllowUserToResizeRows = false;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.FileName,
             this.FilePath});
             this.dataGridView1.Location = new System.Drawing.Point(25, 64);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dataGridView1.Size = new System.Drawing.Size(630, 251);
             this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragDrop);
+            this.dataGridView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.dataGridView1_DragEnter);
             // 
             // label1
             // 
@@ -76,7 +86,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(673, 172);
+            this.button2.Location = new System.Drawing.Point(673, 273);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(115, 42);
             this.button2.TabIndex = 2;
@@ -103,23 +113,72 @@
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(673, 382);
+            this.button4.Font = new System.Drawing.Font("MS UI Gothic", 12F);
+            this.button4.Location = new System.Drawing.Point(570, 382);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(115, 56);
+            this.button4.Size = new System.Drawing.Size(218, 56);
             this.button4.TabIndex = 2;
             this.button4.Text = "結合・出力";
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // FileName
+            // 
+            this.FileName.HeaderText = "ファイル名";
+            this.FileName.Name = "FileName";
+            this.FileName.ReadOnly = true;
+            this.FileName.Width = 150;
             // 
             // FilePath
             // 
             this.FilePath.HeaderText = "ファイルパス";
             this.FilePath.Name = "FilePath";
             this.FilePath.ReadOnly = true;
-            this.FilePath.Width = 400;
+            this.FilePath.Width = 350;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(673, 121);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(115, 30);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "▲一番上へ";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(673, 157);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(115, 30);
+            this.button6.TabIndex = 2;
+            this.button6.Text = "△一行上へ";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(673, 193);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(115, 30);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "▽一行下へ";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(673, 229);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(115, 30);
+            this.button8.TabIndex = 2;
+            this.button8.Text = "▼一番下へ";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // Form1
             // 
+            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
@@ -127,6 +186,10 @@
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
@@ -148,7 +211,12 @@
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn FileName;
         private System.Windows.Forms.DataGridViewTextBoxColumn FilePath;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
     }
 }
 
--- PDFmerger/Form1.cs
+++ PDFmerger/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -31,11 +32,12 @@ namespace PDFmerger
         {
             button1.Enabled = false;
             string[] selectedPaths = new string[1];
+            // ファイルを選択し、それをすべてDataGridViewに入れる
             if (UIpg.openFileSelect(textBox1.Text, ref selectedPaths))
             {
-                foreach(string selectedPath in selectedPaths)
+                foreach (string selectedPath in selectedPaths)
                 {
-                    dataGridView1.Rows.Add(selectedPath);
+                    dataGridView1.Rows.Add(Path.GetFileName(selectedPath), selectedPath);
                 }
             }
             button1.Enabled = true;
@@ -49,6 +51,11 @@ namespace PDFmerger
         private void button2_Click(object sender, EventArgs e)
         {
             button2.Enabled = false;
+            //DataGridView1で選択されているすべての行を削除する
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                dataGridView1.Rows.Remove(row);
+            }
             button2.Enabled = true;
         }
 
@@ -74,6 +81,123 @@ namespace PDFmerger
         {
             button4.Enabled = false;
             button4.Enabled = true;
+        }
+
+        /// <summary>
+        /// ファイルドラッグアンドドロップ時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_DragDrop(object sender, DragEventArgs e)
+        {
+            // ドロップされたファイルをPDFファイルのみすべてDataGridViewに入れる
+            foreach (string selectedPath in (string[])e.Data.GetData(DataFormats.FileDrop, false))
+            {
+                if(Path.GetExtension(selectedPath) == ".pdf")
+                dataGridView1.Rows.Add(Path.GetFileName(selectedPath), selectedPath);
+            }
+        }
+
+        /// <summary>
+        /// DataGridView領域内にマウスポインタが入った際の処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void dataGridView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop))
+            {
+                // エクスプローラのファイルドロップであればドロップ受付
+                e.Effect = DragDropEffects.All;
+            }
+            else
+            {
+                // 上記以外ならドロップ拒否
+                e.Effect = DragDropEffects.None;
+            }
+        }
+
+        /// <summary>
+        /// ▲一番上へボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+            List<DataGridViewRow> buffer = new List<DataGridViewRow>();
+            // バッファに選択行を入れる + 一度削除
+            foreach(DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                buffer.Add(row);
+                dataGridView1.Rows.Remove(row);
+            }
+            // バッファのデータを挿入していく
+            for(int i = 0;i < buffer.Count; i++)
+            {
+                dataGridView1.Rows.Insert(i, buffer[i]);
+            }
+            button5.Enabled = true;
+        }
+
+        /// <summary>
+        /// △一行上へボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button6_Click(object sender, EventArgs e)
+        {
+            button6.Enabled = false;
+            //// 挿入index決定
+            //int insindex = Math.Max(dataGridView1.SelectedRows[0].Index, 0);
+            //List<DataGridViewRow> buffer = new List<DataGridViewRow>();
+            //// バッファに選択行を入れる + 一度削除
+            //foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            //{
+            //    buffer.Add(row);
+            //    dataGridView1.Rows.Remove(row);
+            //}
+            //// バッファのデータを挿入していく
+            //for (int i = 0; i < buffer.Count; i++)
+            //{
+            //    dataGridView1.Rows.Insert(insindex + i, buffer[i]);
+            //}
+            button6.Enabled = true;
+        }
+
+        /// <summary>
+        /// ▽一行下へボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button7_Click(object sender, EventArgs e)
+        {
+            button7.Enabled = false;
+            button7.Enabled = true;
+        }
+
+        /// <summary>
+        /// ▼一番下へボタン押下時処理
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void button8_Click(object sender, EventArgs e)
+        {
+            button8.Enabled = false;
+            List<DataGridViewRow> buffer = new List<DataGridViewRow>();
+            // バッファに選択行を入れる + 一度削除
+            foreach (DataGridViewRow row in dataGridView1.SelectedRows)
+            {
+                buffer.Add(row);
+                dataGridView1.Rows.Remove(row);
+            }
+            // バッファのデータを挿入していく
+            int rowcount = dataGridView1.RowCount;
+            for (int i = 0; i < buffer.Count; i++)
+            {
+                dataGridView1.Rows.Insert(rowcount + i, buffer[i]);
+            }
+            button8.Enabled = true;
         }
     }
 }
